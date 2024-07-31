20951d01ed9b1d536bc0b423539cf0d8895db2ad
Dr-vegapunk <Ishankarki7624@gmail.com>
2024-07-31 07:41:10 +0545

    q2 solved

---
+++ javascript/array/level1.js
@@ -0,0 +1,11 @@
+const arr = [3,4,5]
+//Q1 
+// find the length of the array
+//expected output is 3
+
+console.log(arr.length);
+
+//Q2 
+//reverse the array
+//expected output is [5,4,3]
+console.log(arr.reverse());
