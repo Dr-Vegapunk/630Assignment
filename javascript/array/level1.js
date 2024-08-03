d84cd54cdde39594933589e90ce4b73174e34889
Dr-vegapunk <Ishankarki7624@gmail.com>
2024-08-03 16:40:47 +0545

    solved one filter related question

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -1,11 +1,34 @@
 const arr = [3,4,5]
-//Q1 
-// find the length of the array
-//expected output is 3
+// //Q1 
+// // find the length of the array
+// //expected output is 3
 
 console.log(arr.length);
 
-//Q2 
-//reverse the array
-//expected output is [5,4,3]
+// //Q2 
+// //reverse the array
+// //expected output is [5,4,3]
 console.log(arr.reverse());
+
+//Q1 find list of numbers divisible by 3
+
+const arrr = [3,5,1,2,9,12]
+//write your code here
+const filtArrr=arrr.filter((item)=>{
+    // if (item % 3 ===0){
+    //     return item;
+    // }
+    return item % 3 === 0;
+});
+console.log(filtArrr);
+
+//expected output---> [3,9,12]
+// hint: %3 === 0
+// filter
+//syntax:
+//const filteredArr = arr.filter((item,id)=>{
+//     //
+// })
+//console.log(filteredArr)
+
+//----------------
